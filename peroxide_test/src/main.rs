7b6ad9515b15ef394ad64859d4e39e32594c616c
axect <edeftg@gmail.com>
2019-03-07 03:28:02 +0900

    FP finished

--- peroxide_test/src/main.rs
+++ peroxide_test/src/main.rs
@@ -2,6 +2,15 @@ extern crate peroxide;
 use peroxide::*;
 
 fn main() {
-    let a = matrix!(1;4;1, 2, 2, Row);
-    a.inv().unwrap().print();
+    let a = c!(1,2,3,4);
+    let b = c!(5,6,7,8);
+
+    // Original rust
+    a.clone()
+        .into_iter()
+        .zip(&b)
+        .map(|(x, y)| x + *y)
+        .collect::<Vec<f64>>().print();
+
+    a.zip_with(|x, y| x + y, &b).print();
 }
